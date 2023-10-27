ab307c4f92e6df837150f43d5c65038ed39a4eb3
gnkz <gnkz.dev@gmail.com>
2023-10-27 14:23:21 -0600

    fix: update cast command

--- src/Surl.sol
+++ src/Surl.sol
@@ -88,7 +88,7 @@ library Surl {
         returns (uint256 status, bytes memory data)
     {
         string memory scriptStart = 'response=$(curl -s -w "\\n%{http_code}" ';
-        string memory scriptEnd = '); status=$(tail -n1 <<< "$response"); data=$(sed "$ d" <<< "$response");data=$(echo "$data" | tr -d "\\n"); cast abi-encode "response(uint256,string)" "$status" "$data";';
+        string memory scriptEnd = '); status=$(tail -n1 <<< "$response"); data=$(sed "$ d" <<< "$response");data=$(echo "$data" | tr -d "\\n"); cast abi-encode "response(uint256,string)" "$status" \\\'"$data"\\\';';
 
         string memory curlParams = "";
 
